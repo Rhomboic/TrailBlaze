d61eb1301389c7513bc78b6e75327573bfe3d70f
Rhomboic <lynxdroid2@gmail.com>
2022-07-01 12:20:41 -0700

    configured sign up with alert for empty required fields

--- TrailBlaze/View Controllers/RegisterViewController.m
+++ TrailBlaze/View Controllers/RegisterViewController.m
@@ -9,6 +9,10 @@
 #import "Parse/Parse.h"
 
 @interface RegisterViewController ()
+@property (weak, nonatomic) IBOutlet UITextField *usernameField;
+@property (weak, nonatomic) IBOutlet UITextField *emailField;
+@property (weak, nonatomic) IBOutlet UITextField *passwordField;
+@property UIAlertController *alert;
 
 
 @end
@@ -18,6 +22,36 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    self.alert = [UIAlertController alertControllerWithTitle:@"Empty Fields" message:@"Please fill all fields" preferredStyle:(UIAlertControllerStyleAlert)];
+    UIAlertAction *okAction = [UIAlertAction actionWithTitle:@"OK" style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {}];
+
+    [self.alert addAction:okAction];
+}
+- (IBAction)didTapSignup:(id)sender {
+    if ([_usernameField.text isEqual:@""] || [_passwordField.text isEqual:@""] || [_emailField.text isEqual:@""]) {
+        [self presentViewController:self.alert animated:YES completion:^{}];
+    } else {
+        [self registerUser];
+    }
+}
+
+- (void)registerUser {
+    
+    PFUser *newUser = [PFUser user];
+    
+    newUser.username = self.usernameField.text;
+
+    newUser.password = self.passwordField.text;
+    
+    newUser.email = self.emailField.text;
+    
+    [newUser signUpInBackgroundWithBlock:^(BOOL succeeded, NSError * error) {
+        if (error != nil) {
+            NSLog(@"Error: %@", error.localizedDescription);
+        } else {
+            NSLog(@"User registered successfully");
+        }
+    }];
 }
 
 /*
