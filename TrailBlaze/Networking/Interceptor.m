a386e5f465cd8d324955a168bff039a3a34b9398
Rhomboic <lynxdroid2@gmail.com>
2022-07-20 23:59:27 -0700

    addded async function to request ETAs for both users to all the points and return the point with the least ETA difference

--- TrailBlaze/Networking/Interceptor.m
+++ TrailBlaze/Networking/Interceptor.m
@@ -49,4 +49,67 @@ static NSMutableDictionary *etaPointPairs;
     return prunedPoints;
 }
 
++ (void) getBestETAPoint: (NSArray *) allPoints interceptorLocation: (CLLocation *) interceptorLocation runnerLocation: (CLLocation *) runnerLocation completion:(void (^)(MKMapItem *bestPoint, NSError *))completion {
+    
+    NSArray *prunedPoints = [self prunePoints:allPoints numberOfPoints:10];
+    etas = [NSMutableArray array];
+    etaPointPairs = [[NSMutableDictionary alloc] init];
+    dispatch_queue_t serialQueue = dispatch_queue_create("serialQueue", nil);
+    dispatch_group_t group = dispatch_group_create();
+    
+    MKPlacemark *startInterceptorPlacemark = [[MKPlacemark alloc] initWithCoordinate:CLLocationCoordinate2DMake(interceptorLocation.coordinate.latitude, interceptorLocation.coordinate.longitude)];
+    
+    MKPlacemark *startRunnerPlacemark = [[MKPlacemark alloc] initWithCoordinate:CLLocationCoordinate2DMake(runnerLocation.coordinate.latitude, runnerLocation.coordinate.longitude)];
+    
+    MKMapItem *startInterceptorItem = [[MKMapItem alloc] initWithPlacemark:startInterceptorPlacemark];
+    
+    MKMapItem *startRunnerItem = [[MKMapItem alloc] initWithPlacemark:startRunnerPlacemark];
+
+    for (int i=0; i < prunedPoints.count; i++) {
+        NSLog(@"starting operation %@..", @(i));
+        
+        dispatch_group_enter(group);
+        MKPlacemark *destinationPlacemark = [[MKPlacemark alloc] initWithCoordinate:CLLocationCoordinate2DMake([[prunedPoints objectAtIndex:i][0] doubleValue], [[prunedPoints objectAtIndex:i][1] doubleValue])];
+
+        MKMapItem *destinationItem =  [[MKMapItem alloc] initWithPlacemark:destinationPlacemark];
+
+        MKDirectionsRequest *pathInterceptorRequest = [[MKDirectionsRequest alloc] init];
+        [pathInterceptorRequest setSource:startInterceptorItem];
+        [pathInterceptorRequest setDestination:destinationItem];
+        [pathInterceptorRequest setTransportType:MKDirectionsTransportTypeWalking];
+        [pathInterceptorRequest setRequestsAlternateRoutes:NO];
+        
+        MKDirectionsRequest *pathRunnerRequest = [[MKDirectionsRequest alloc] init];
+        [pathRunnerRequest setSource:startRunnerItem];
+        [pathRunnerRequest setDestination:destinationItem];
+        [pathRunnerRequest setTransportType:MKDirectionsTransportTypeWalking];
+        [pathRunnerRequest setRequestsAlternateRoutes:NO];
+        
+        MKDirections *pathInterceptor = [[MKDirections alloc] initWithRequest:pathInterceptorRequest];
+        MKDirections *pathRunner = [[MKDirections alloc] initWithRequest:pathRunnerRequest];
+        [pathInterceptor calculateETAWithCompletionHandler:^(MKETAResponse * _Nullable interceptorResponse, NSError * _Nullable error1) {
+            if (interceptorResponse) {
+                [pathRunner calculateETAWithCompletionHandler:^(MKETAResponse * _Nullable runnerResponse, NSError * _Nullable error2) {
+                    if (runnerResponse) {
+                        [etas addObject:[NSNumber numberWithDouble:fabs(interceptorResponse.expectedTravelTime - runnerResponse.expectedTravelTime)]];
+                        [etaPointPairs setValue:runnerResponse.destination forKey:[NSString stringWithFormat:@"%.f", fabs(interceptorResponse.expectedTravelTime - runnerResponse.expectedTravelTime)]];
+                    } else {
+                        NSLog(@"%@", error2.localizedDescription);
+                    }
+                    dispatch_async(serialQueue, ^{
+                        dispatch_group_leave(group);
+                    });
+                }];
+            } else {
+                NSLog(@"%@", error1.localizedDescription);
+            }
+        }];
+    }
+    dispatch_group_notify(group, dispatch_get_main_queue(), ^{
+        NSLog(@"everything is done!!");
+        completion([etaPointPairs objectForKey:[NSString stringWithFormat:@"%@", [[self sortInAscendingOrder:etas] firstObject]]], nil);
+    });
+}
+
+
 @end
