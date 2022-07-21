b679ea2164b71993a4508c29ad40a214d60ff14d
Rhomboic <lynxdroid2@gmail.com>
2022-07-21 00:00:43 -0700

    added function headers to headerfile'

--- TrailBlaze/Networking/Interceptor.h
+++ TrailBlaze/Networking/Interceptor.h
@@ -6,11 +6,13 @@
 //
 
 #import <Foundation/Foundation.h>
+#import "CoreLocation/CoreLocation.h"
+#import "MapKit/MapKit.h"
 
 NS_ASSUME_NONNULL_BEGIN
 
 @interface Interceptor : NSObject
-
++ (void) getBestETAPoint: (NSArray *) allPoints interceptorLocation: (CLLocation *) interceptorLocation runnerLocation: (CLLocation *) runnerLocation completion:(void (^)(MKMapItem *bestPoint, NSError *))completion;
 @end
 
 NS_ASSUME_NONNULL_END
