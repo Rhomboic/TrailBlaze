f346b88115c79e3d2fdabde80f1464b925dfcae6
Rhomboic <lynxdroid2@gmail.com>
2022-07-26 16:45:05 -0700

    added function header to headerfile

--- TrailBlaze/Objects/Interception.h
+++ TrailBlaze/Objects/Interception.h
@@ -6,11 +6,15 @@
 //
 
 #import <Foundation/Foundation.h>
+@import Parse;
 
 NS_ASSUME_NONNULL_BEGIN
 
-@interface Interception : NSObject
-
+@interface Interception : PFObject<PFSubclassing>
+@property (nonatomic, strong) PFGeoPoint *rendezvous;
+@property (nonatomic, strong) NSString *polylineCoords;
+@property (nonatomic, strong) NSString *startTime;
++ (void) uploadRequest: (PFGeoPoint *) rendezvous polylineCoords: (NSString *)polylineCoords startTime: (NSString *) startTime withCompletion: (PFBooleanResultBlock _Nullable)completion;
 @end
 
 NS_ASSUME_NONNULL_END
