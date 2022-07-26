d8bbc60131e60a26f8e187320cd38e753869e7d1
Rhomboic <lynxdroid2@gmail.com>
2022-07-26 16:44:45 -0700

    created model with function to upload Interceptions

--- TrailBlaze/Objects/Interception.m
+++ TrailBlaze/Objects/Interception.m
@@ -6,7 +6,30 @@
 //
 
 #import "Interception.h"
+@import Parse;
 
 @implementation Interception
+@dynamic rendezvous;
+@dynamic polylineCoords;
+@dynamic startTime;
 
++ (nonnull NSString *)parseClassName {
+    return @"Interception";
+}
+
++ (void) uploadRequest: (PFGeoPoint *) rendezvous polylineCoords: (NSString *)polylineCoords startTime: (NSString *) startTime withCompletion: (PFBooleanResultBlock _Nullable)completion {
+    
+    Interception *newReq = [Interception new];
+    [newReq setRendezvous:rendezvous];
+    [newReq setPolylineCoords:polylineCoords];
+    [newReq setStartTime:startTime];
+
+    [newReq saveInBackgroundWithBlock:^(BOOL succeeded, NSError * _Nullable error) {
+        if (error) {
+            NSLog(@"%@", error.localizedDescription);
+        } else {
+            NSLog(@"Successfully sent intercept request!");
+        }
+    }];
+}
 @end
