051adc0f068416cd8e1862433d8ca27dbfa58f4a
Rhomboic <lynxdroid2@gmail.com>
2022-07-26 16:33:04 -0700

    created model files for interception path

---
+++ TrailBlaze/Objects/Interception.h
@@ -0,0 +1,16 @@
+//
+//  Interception.h
+//  TrailBlaze
+//
+//  Created by Adam Issah on 7/26/22.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface Interception : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ TrailBlaze/Objects/Interception.m
@@ -0,0 +1,12 @@
+//
+//  Interception.m
+//  TrailBlaze
+//
+//  Created by Adam Issah on 7/26/22.
+//
+
+#import "Interception.h"
+
+@implementation Interception
+
+@end
