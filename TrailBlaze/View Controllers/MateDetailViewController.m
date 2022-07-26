e4a1615854d81302fb902fa5e95f319acd1a2808
Rhomboic <lynxdroid2@gmail.com>
2022-07-26 15:22:59 -0700

    implemented live query to check if intercept request is approved

--- TrailBlaze/View Controllers/MateDetailViewController.m
+++ TrailBlaze/View Controllers/MateDetailViewController.m
@@ -11,6 +11,7 @@
 #import "HomeViewController.h"
 #import "InterceptRequest.h"
 #import "UIImageView+AFNetworking.h"
+#import "ParseLiveQuery/ParseLiveQuery-umbrella.h"
 
 @interface MateDetailViewController ()
 @property (weak, nonatomic) IBOutlet UIButton *interceptButton;
@@ -19,9 +20,14 @@
 
 @end
 
-@implementation MateDetailViewController
+@implementation MateDetailViewController {
+    PFLiveQueryClient *liveQueryClient;
+    PFLiveQuerySubscription *liveQuerySubscription;
+    PFQuery *approvalQuery;
+}
 - (void)viewDidLoad {
     [super viewDidLoad];
+    [self parseLiveQuerySetUp];
     self.profileName.text = _thisUser.username;
     PFFileObject *image = [_thisUser objectForKey:@"profileImage"];
     [_profilePhoto setImageWithURL:[NSURL URLWithString:[image url]]];
@@ -36,22 +42,41 @@
             NSLog(@"failed to send intercept request");
         }
     }];
-     [Run retreiveRunPolyline:_thisUser completion:^(MKPolyline * _Nonnull polyline, NSError * _Nullable err) {
-        if (polyline) {
-            SceneDelegate *sceneDelegate = (SceneDelegate *)UIApplication.sharedApplication.connectedScenes.allObjects.firstObject.delegate;
-            UIStoryboard *storyboard = [UIStoryboard storyboardWithName:@"Main" bundle:nil];
-            UITabBarController *tabBarController = [storyboard instantiateViewControllerWithIdentifier:@"TabBarController"];
-            UINavigationController *navController = tabBarController.viewControllers[1];
-            HomeViewController *hvc = navController.childViewControllers[0];
-            hvc.cloudPolyline = polyline;
-            hvc.cloudUser = self->_thisUser;
-            [tabBarController setSelectedViewController: navController];
-            sceneDelegate.window.rootViewController = tabBarController;
-            NSLog(@"got run");
-            
-        } else {
-            NSLog(@"did not get run");
-        }
+     
+}
+
+- (void) parseLiveQuerySetUp {
+    NSString *path = [[NSBundle mainBundle] pathForResource: @"keys" ofType: @"plist"];
+    NSDictionary *dict = [NSDictionary dictionaryWithContentsOfFile: path];
+
+    NSString *appID = [dict objectForKey: @"appID"];
+    NSString *clientKey = [dict objectForKey: @"clientKey"];
+    liveQueryClient = [[PFLiveQueryClient alloc] initWithServer:@"https://tblaze.b4a.io" applicationId:appID clientKey:clientKey];
+    approvalQuery = [PFQuery queryWithClassName:@"InterceptRequest"];
+    [approvalQuery whereKey:@"approved" equalTo: [NSNumber numberWithBool:YES]];
+    [approvalQuery whereKey:@"requester" equalTo: PFUser.currentUser.objectId];
+    liveQuerySubscription = [liveQueryClient subscribeToQuery:approvalQuery];
+    __weak typeof(self) weakself = self;
+    [liveQuerySubscription addUpdateHandler:^(PFQuery<PFObject *> * _Nonnull query, PFObject * _Nonnull object) {
+        __strong typeof(self) strongself = weakself;
+        [Run retreiveRunPolyline:strongself->_thisUser completion:^(MKPolyline * _Nonnull polyline, NSError * _Nullable err) {
+           if (polyline) {
+               SceneDelegate *sceneDelegate = (SceneDelegate *)UIApplication.sharedApplication.connectedScenes.allObjects.firstObject.delegate;
+               UIStoryboard *storyboard = [UIStoryboard storyboardWithName:@"Main" bundle:nil];
+               UITabBarController *tabBarController = [storyboard instantiateViewControllerWithIdentifier:@"TabBarController"];
+               UINavigationController *navController = tabBarController.viewControllers[1];
+               HomeViewController *hvc = navController.childViewControllers[0];
+               hvc.cloudPolyline = polyline;
+               hvc.cloudUser = strongself->_thisUser;
+               [tabBarController setSelectedViewController: navController];
+               sceneDelegate.window.rootViewController = tabBarController;
+               NSLog(@"got run");
+               //stop activity indicator animation
+           } else {
+               NSLog(@"did not get run");
+           }
+       }];
+    
     }];
 }
 @end
