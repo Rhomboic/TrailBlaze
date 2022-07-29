6c60e14abcdd11adf09f434ce73f4b882ecbe7be
Rhomboic <lynxdroid2@gmail.com>
2022-07-29 11:49:55 -0700

    created date formatting function

--- TrailBlaze/Objects/Utils.m
+++ TrailBlaze/Objects/Utils.m
@@ -8,5 +8,11 @@
 #import "Utils.h"
 
 @implementation Utils
++ (NSString *) currentDateTime {
+    NSDateFormatter *DateFormatter=[[NSDateFormatter alloc] init];
+    [DateFormatter setDateFormat:@"yyyy-MM-dd hh:mm:ss"];
+    return [DateFormatter stringFromDate:[NSDate date]];
+}
+
 
 @end
