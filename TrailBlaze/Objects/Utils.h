95cb6c8fa142e1d0027706913d2dd7d89630c470
Rhomboic <lynxdroid2@gmail.com>
2022-07-29 11:48:59 -0700

    created shared utility clas

---
+++ TrailBlaze/Objects/Utils.h
@@ -0,0 +1,16 @@
+//
+//  Utils.h
+//  TrailBlaze
+//
+//  Created by Adam Issah on 7/29/22.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface Utils : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ TrailBlaze/Objects/Utils.m
@@ -0,0 +1,12 @@
+//
+//  Utils.m
+//  TrailBlaze
+//
+//  Created by Adam Issah on 7/29/22.
+//
+
+#import "Utils.h"
+
+@implementation Utils
+
+@end
