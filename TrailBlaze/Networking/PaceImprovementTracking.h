a0fe5748a5c76a8caecd4c06e5488005f4ef3fda
Rhomboic <lynxdroid2@gmail.com>
2022-08-01 12:13:26 -0700

    created function to tell if user is within 10 meters of start point

--- TrailBlaze/Networking/PaceImprovementTracking.h
+++ TrailBlaze/Networking/PaceImprovementTracking.h
@@ -6,12 +6,22 @@
 //
 
 #import <Foundation/Foundation.h>
+#import "CoreLocation/CoreLocation.h"
 
 NS_ASSUME_NONNULL_BEGIN
 
 @interface PaceImprovementTracking : NSObject
++ (BOOL) isAtStartPosition: (CLLocation *) userLocation firstPoint: (NSArray *) firstPolylinePoint;
 
 @end
 
 NS_ASSUME_NONNULL_END
+
+//
+//  PIT.m
+//  TrailBlaze
+//
+//  Created by Adam Issah on 7/29/22.
+//
+
 
--- TrailBlaze/Networking/PaceImprovementTracking.m
+++ TrailBlaze/Networking/PaceImprovementTracking.m
@@ -6,7 +6,19 @@
 //
 
 #import "PaceImprovementTracking.h"
+#import "CoreLocation/CoreLocation.h"
 
 @implementation PaceImprovementTracking
+
++ (BOOL) isAtStartPosition: (CLLocation *) userLocation firstPoint: (NSArray *) firstPolylinePoint {
+    CLLocation *firstPointLocation = [[CLLocation alloc] initWithLatitude:[firstPolylinePoint[0] doubleValue] longitude:[firstPolylinePoint[1] doubleValue]];
+    if ([firstPointLocation distanceFromLocation:userLocation] < 10) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+
 
 @end
