f61cd107e13cbb6b9956eaf6f313d4e779fb9afd
Rhomboic <lynxdroid2@gmail.com>
2022-08-01 12:05:02 -0700

    created class for PIT feature

---
+++ TrailBlaze/Networking/PaceImprovementTracking.h
@@ -0,0 +1,17 @@
+//
+//  PaceImprovementTracking.h
+//  TrailBlaze
+//
+//  Created by Adam Issah on 8/1/22.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface PaceImprovementTracking : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
+
---
+++ TrailBlaze/Networking/PaceImprovementTracking.m
@@ -0,0 +1,12 @@
+//
+//  PaceImprovementTracking.m
+//  TrailBlaze
+//
+//  Created by Adam Issah on 8/1/22.
+//
+
+#import "PaceImprovementTracking.h"
+
+@implementation PaceImprovementTracking
+
+@end
