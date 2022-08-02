3dbe7abf168ccdf29928b36ba1bf7279a0e28a98
Rhomboic <lynxdroid2@gmail.com>
2022-08-02 10:50:27 -0700

    [5/x] PIT - (refactoring) added jsonStringToArray function header to Utils headerfile

--- TrailBlaze/Objects/Utils.h
+++ TrailBlaze/Objects/Utils.h
@@ -7,6 +7,7 @@
 
 #import <Foundation/Foundation.h>
 #import "MapKit/MapKit.h"
+@import Parse;
 
 NS_ASSUME_NONNULL_BEGIN
 
@@ -14,6 +15,7 @@ NS_ASSUME_NONNULL_BEGIN
 
 + (NSString *) currentDateTime;
 + (NSString *) arrayToJSONString: (MKPolyline *) route;
++ (NSArray *) jsonStringToArray: (PFObject *) object;
 
 @end
 
