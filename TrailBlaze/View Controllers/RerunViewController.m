979189fee48dfdc783c26333b54f3970017d2664
Rhomboic <lynxdroid2@gmail.com>
2022-08-03 15:32:28 -0700

    [15/x][PR3] PIT - set up table view similar to profile view controller table view

--- TrailBlaze/View Controllers/RerunViewController.m
+++ TrailBlaze/View Controllers/RerunViewController.m
@@ -6,26 +6,71 @@
 //
 
 #import "RerunViewController.h"
+#import "Run.h"
+#import "RerunCell.h"
 
-@interface RerunViewController ()
+@interface RerunViewController () <UITableViewDataSource, UITableViewDelegate>
+@property (weak, nonatomic) IBOutlet UITableView *tableView;
 
 @end
 
-@implementation RerunViewController
-
+@implementation RerunViewController {
+    NSArray *pastRuns;
+}
+- (void) viewWillAppear:(BOOL)animated {
+    [Run retreiveRunObjects:PFUser.currentUser limit:10 completion:^(NSArray * _Nonnull runObjects, NSError * _Nullable err) {
+        if (runObjects) {
+            self->pastRuns = runObjects;
+            [self->_tableView reloadData];
+        } else {
+            NSLog(@"No past runs");
+        }
+    }];
+}
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    self.tableView.dataSource = self;
+    [[PFUser currentUser][@"pastRuns"] fetchIfNeeded];
+    pastRuns = [PFUser currentUser][@"pastRuns"];
+    
+    PFFileObject *image = [PFUser.currentUser objectForKey:@"profileImage"];
+    NSLog(@"%@",image.url);
+    
+    [Run retreiveRunObjects:PFUser.currentUser limit:20 completion:^(NSArray * _Nonnull runObjects, NSError * _Nullable err) {
+        if (runObjects) {
+            self->pastRuns = runObjects;
+            [self->_tableView reloadData];
+        } else {
+            NSLog(@"No past runs");
+        }
+    }];
+    
+    
 }
 
-/*
-#pragma mark - Navigation
 
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+- (nonnull UITableViewCell *)tableView:(nonnull UITableView *)tableView cellForRowAtIndexPath:(nonnull NSIndexPath *)indexPath {
+    RerunCell *cell = [tableView dequeueReusableCellWithIdentifier:@"RerunCell" forIndexPath:indexPath];
+    if (pastRuns) {
+        @try {
+        cell.startLocationAddress.text = pastRuns[indexPath.row][@"startLocationAddress"];
+        cell.endLocationAddress.text = pastRuns[indexPath.row][@"endLocationAddress"];
+        cell.averagePace.text = pastRuns[indexPath.row][@"overallAveragePace"];
+        }
+        @catch (NSException * e) {
+            
+        }
+        
+        cell.layer.cornerRadius = 20;
+        [cell.layer setBorderColor:[UIColor systemBackgroundColor].CGColor];
+        [cell.layer setBorderWidth:5.0f];
+        cell.clipsToBounds = true;
+        cell.contentView.backgroundColor = UIColor.secondarySystemBackgroundColor;
+    }
+    return cell;
 }
-*/
 
+- (NSInteger)tableView:(nonnull UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return pastRuns.count;
+}
 @end
