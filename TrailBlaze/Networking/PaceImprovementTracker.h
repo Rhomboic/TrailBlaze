0e146a2f69554bd0317134d34f00822b0dc0779e
Rhomboic <lynxdroid2@gmail.com>
2022-08-04 11:42:19 -0700

    [23/x][PR3] PIT - changed paceTracker function signature in headerfile

--- TrailBlaze/Networking/PaceImprovementTracker.h
+++ TrailBlaze/Networking/PaceImprovementTracker.h
@@ -27,7 +27,7 @@ NS_ASSUME_NONNULL_BEGIN
 + (BOOL) isAtStartPosition: (CLLocation *) userLocation firstPoint: (NSArray *) firstPolylinePoint;
 - (BOOL) passedPoint: (NSArray *) nextTwoPoints currentLocation: (CLLocation *) currentLocation;
 - (NSArray *) paceCompare: (NSNumber *) previousIntervalPace currentIntervalPace: (NSNumber *)currentIntervalPace pointsForInterval: (NSArray *) pointsForInterval;
-- (void) paceTracker: (NSArray *) polylinePoints userLocation: (CLLocation *) userLocation bestPaces: (NSDictionary *) bestPaces;
+- (void) paceTracker:(CLLocation *) userLocation;
 - (void) saveImprovedPaceDictionary: (PFObject *) runObject;
 
 @end
